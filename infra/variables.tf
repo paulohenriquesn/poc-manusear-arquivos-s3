57becb8460aaa9fb0220acaacf99529ee53a6399
Paulo Henrique <contato@paulohenriquesn.com>
2026-02-21 17:20:10 -0300

    fix: envs not used

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,12 +1,3 @@
-variable "state_bucket_name" {
-  description = "Name of the S3 bucket for storing Terraform state"
-  type = string
-}
-variable "state_key_file" {
-  description = "state key file"
-  type = string
-}
-
 variable "aws_region" {
   description = "AWS region"
   type = string
