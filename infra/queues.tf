a660c471e4920dd9d835359d2ddad49ed448283d
Paulo Henrique <contato@paulohenriquesn.com>
2026-02-21 17:42:33 -0300

    fix: queue timeout

--- infra/queues.tf
+++ infra/queues.tf
@@ -4,4 +4,5 @@ resource "aws_sqs_queue" "processar-arquivos-queue" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
+  visibility_timeout_seconds = 360
 }
